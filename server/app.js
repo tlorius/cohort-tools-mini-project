8ae3acaf8fe8cdffa10afc08c3ab5111f81987a1
Tom Lorius <tomlorius.mail@gmail.com>
2024-01-16 16:57:46 +0100

    finished some day 3 routes

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
+const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const PORT = 5005;
 
@@ -8,9 +9,18 @@ const PORT = 5005;
 // Devs Team - Import the provided files with JSON data of students and cohorts here:
 const cohorts = require("./cohorts.json");
 const students = require("./students.json");
+const Student = require("./models/Student.model");
+const Cohort = require("./models/Cohort.model");
 
 // INITIALIZE EXPRESS APP - https://expressjs.com/en/4x/api.html#express
 const app = express();
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/cohort-tools-api")
+  .then((x) =>
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
+  )
+  .catch((err) => console.error("Error connecting to mongo", err));
 
 // MIDDLEWARE
 // Research Team - Set up CORS middleware here:
@@ -34,11 +44,51 @@ app.get("/docs", (req, res) => {
 });
 
 app.get("/api/cohorts", (req, res) => {
-  res.json(cohorts);
+  Cohort.find({})
+    .then((cohorts) => {
+      res.status(200).send(cohorts);
+    })
+    .catch((error) => {
+      console.error("Error while retrieving cohorts ->", error);
+      res.status(500).send({ error: "Failed to retrieve cohorts" });
+    });
 });
 
 app.get("/api/students", (req, res) => {
-  res.json(students);
+  Student.find({})
+    .then((students) => {
+      res.status(200).send(students);
+    })
+    .catch((error) => {
+      console.error("Error while retrieving studnets ->", error);
+      res.status(500).send({ error: "Failed to retrieve students" });
+    });
+});
+
+app.post("/api/students", (req, res) => {
+  //i guess we can use this shorthand version if we know the request matches the schema
+  Student.create(req.body)
+    .then((createdStudent) => {
+      //remove this log again if it works
+      console.log("Student created ->", createdStudent);
+      res.status(201).send(createdStudent);
+    })
+    .catch((error) => {
+      console.error("Error while creating the student ->", error);
+      res.status(500).send({ error: "Failed to create the student" });
+    });
+});
+
+app.get("/api/students/cohort/:cohortId", (req, res) => {
+  const cohortId = req.params.cohortId;
+  Student.find({ cohort: cohortId })
+    .then((students) => {
+      res.status(200).send(students);
+    })
+    .catch((error) => {
+      console.error("Error while retrieving studnets ->", error);
+      res.status(500).send({ error: "Failed to retrieve students" });
+    });
 });
 
 // START SERVER
